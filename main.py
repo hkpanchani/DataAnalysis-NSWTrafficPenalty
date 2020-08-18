1e2721c63145a1833fb71a1e8c201f0c3f39afb9
Hiren Panchani <hiren_panchani@yahoo.com>
2020-08-18 20:55:46 +0530

    GUI Elements added - Initialised with empty window

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+import sys
+from PyQt5 import QtWidgets,QtCore, QtGui
+from PyQt5.QtWidgets import QInputDialog, QFileDialog, QMessageBox, QButtonGroup
+from view import Ui_MainWindow
 from dataModel import Dataset
 
-class Main:
-    pass
+class Main(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
 
 
 
 if __name__ == "__main__":
     file = 'dataset\\dataset.csv'
     ds = Dataset(filepath=file)
-    ds.dateFilter(start_date='1/3/2012',end_date='1/5/2012')
+    df = ds.dateFilter(start_date='1/3/2012',end_date='1/5/2012')
+    print(df)
+
+    # To be used when GUI Window is ready
+    # app = QtWidgets.QApplication(sys.argv)
+    # window = Main()
+    # window.show()
+    # sys.exit(app.exec_())
+
+    

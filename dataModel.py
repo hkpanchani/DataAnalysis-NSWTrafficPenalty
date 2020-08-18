e09f39e6e75bb5ee24fada5126474ae111c3aab9
Hiren Panchani <hiren_panchani@yahoo.com>
2020-08-18 20:03:02 +0530

    created function to return rows between specified date

--- dataModel.py
+++ dataModel.py
@@ -2,6 +2,17 @@ import sys
 import pandas as pd
 
 class Dataset:
+
+    df = None
+    
+    # Initialise the Dataframe and 
     def __init__(self,filepath):
-        df = pd.read_csv(filepath)
-        print(df.head(20))
+        self.df = pd.read_csv(filepath)
+        self.df['OFFENCE_MONTH'] = pd.to_datetime(self.df['OFFENCE_MONTH'])
+
+
+    # Return the Dataframe of records in specified Time Period
+    def dateFilter(self,start_date,end_date):
+        mask = (self.df['OFFENCE_MONTH'] >= start_date) & (self.df['OFFENCE_MONTH'] < end_date)
+        df = self.df.loc[mask]
+        return df
--- main.py
+++ main.py
@@ -8,3 +8,4 @@ class Main:
 if __name__ == "__main__":
     file = 'dataset\\dataset.csv'
     ds = Dataset(filepath=file)
+    ds.dateFilter(start_date='1/3/2012',end_date='1/5/2012')

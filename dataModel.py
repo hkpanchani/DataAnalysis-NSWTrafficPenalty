cffa18013c41aed83eecdc881a7afd54fa43abae
Hiren Panchani <hiren_panchani@yahoo.com>
2020-09-22 10:39:15 +0530

    UI with All data initialised

--- dataModel.py
+++ dataModel.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+from PyQt5.QtCore import QAbstractTableModel, Qt
 
 class Dataset:
 
@@ -7,12 +8,42 @@ class Dataset:
     
     # Initialise the Dataframe and 
     def __init__(self,filepath):
-        self.df = pd.read_csv(filepath)
+        self.df = pd.read_csv(filepath,low_memory=False)
         self.df['OFFENCE_MONTH'] = pd.to_datetime(self.df['OFFENCE_MONTH'])
 
+    def getModel(self,dataframe):
+        model = pandasModel(dataframe)
+        return model
+
+    # Returns Model of AN Entire CSV
+    def getAllData(self):
+        return pandasModel(self.ds)
 
     # Return the Dataframe of records in specified Time Period
     def dateFilter(self,start_date,end_date):
         mask = (self.df['OFFENCE_MONTH'] >= start_date) & (self.df['OFFENCE_MONTH'] < end_date)
         df = self.df.loc[mask]
-        return df
+        return self.getModel(df)
+
+
+class pandasModel(QAbstractTableModel):
+    def __init__(self, data):
+        QAbstractTableModel.__init__(self)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parnet=None):
+        return self._data.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return str(self._data.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self._data.columns[col]
+        return None
--- main.py
+++ main.py
@@ -4,25 +4,45 @@ from PyQt5.QtWidgets import QInputDialog, QFileDialog, QMessageBox, QButtonGroup
 from view import Ui_MainWindow
 from dataModel import Dataset
 
+file = 'dataset\\dataset.csv'
+
 class Main(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(Main, self).__init__()
+    def __init__(self,parent=None):
+        super(Main, self).__init__(parent=parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        
+        self.setComboBoxes()
+        self.initialiseTable()
 
+    def setComboBoxes(self):
+        filter_by = ['All Penalty Cases','Camera or Radar Cases','Mobile Phone Cases']
+        analysis_over = ['Over Time','Offence Code','Location Code']
+        
+        # Initialise FilterBy Drop Down 
+        for item in filter_by:
+            self.ui.filterBySpin.addItem(item)
+        
+        # Initialise Analysys Over Drop Down
+        for item in analysis_over:
+            self.ui.analysisOverSpin.addItem(item)
 
+    def initialiseTable(self):
+        # ds.getModel()
+        self.ui.table.setModel(ds.getAllData())
 
+    # def clearTableData(self):
+    #     self.ui.table.clar
 
 if __name__ == "__main__":
-    file = 'dataset\\dataset.csv'
     ds = Dataset(filepath=file)
     df = ds.dateFilter(start_date='1/3/2012',end_date='1/5/2012')
-    print(df)
+    # print(df)
 
     # To be used when GUI Window is ready
-    # app = QtWidgets.QApplication(sys.argv)
-    # window = Main()
-    # window.show()
-    # sys.exit(app.exec_())
+    app = QtWidgets.QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
 
     
